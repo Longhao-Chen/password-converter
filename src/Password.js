fbf08eef8223c6ecf329a935f84d10bd8066bb7b
Longhao.Chen <Longhao.Chen@outlook.com>
2021-05-30 08:35:22 +0800

    新文件：   README.md 新文件：   package.json 新文件：   public/index.html 新文件：   public/manifest.json 新文件：   public/robots.txt 新文件：   src/Password.js 新文件：   src/index.css 新文件：   src/index.js

---
+++ src/Password.js
@@ -0,0 +1,24 @@
+//计算加密后的密码
+const crypto = require('crypto');
+const hash = crypto.createHash('sha512');
+
+function sha512(input) {
+	hash.update(input);
+	return hash.digest('hex');
+}
+
+function Password(input) {
+	var pass = sha512(input);
+	//增加暴力破解的计算量，总共计算50次sha512
+	for (var i = 1; i < 50; ++i) {
+		pass = sha512(pass);
+	}
+
+	//转成Base64和加入特殊字符，防止某些要求高的地方认为密码强度不够
+	var output = Buffer.from(pass, 'utf-8');
+	output = output.toString('base64');
+	output = output.slice(0, 4) + '-' + output.slice(4, 8) + '-' + output.slice(8, 12);
+	return output;
+}
+
+export default Password;
