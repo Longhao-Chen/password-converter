821a63dfb8f5d257b65dba889a6904724a8d2462
Longhao.Chen <Longhao.Chen@outlook.com>
2021-06-02 16:06:41 +0800

    添加说明文档

---
+++ src/Footer.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+function Footer() {
+	return (
+		<dev class="center">
+			<hr />
+			<footer class="footer navbar-fixed-bottom ">
+				<div class="container">
+					项目主页及说明文档：
+					<a href="https://github.com/Longhao-Chen/password-converter/">
+						https://github.com/Longhao-Chen/password-converter/
+					</a>
+				</div>
+			</footer>
+		</dev>
+	);
+}
+
+export default Footer;
