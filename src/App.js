5df1f75328da061d413dc07208406ab77a978649
Longhao.Chen <Longhao.Chen@outlook.com>
2021-06-02 16:14:27 +0800

    添加 清除密码 按钮

--- src/App.js
+++ src/App.js
@@ -7,16 +7,22 @@ class HelloMessage extends React.Component {
 		super(props);
 		this.state = { value: '' };
 		this.handleChange = this.handleChange.bind(this);
+		this.cleanPassword = this.cleanPassword.bind(this);
 	}
 
 	handleChange(event) {
 		this.setState({ value: event.target.value });
+	}
+
+	cleanPassword() {
+		this.setState({ value: '' });
 	}
 	render() {
 		var value = this.state.value;
 		return <div>
 			<p>在此输入您的密码：</p>
 			<input autofocus="autofocus" type="password" value={value} onChange={this.handleChange} />
+			<button onClick={this.cleanPassword}>清除密码</button>
 			<br />
 			<p>转换后的密码：</p>
 			<input readonly="readonly" type="text" value={Password(value)}  />
