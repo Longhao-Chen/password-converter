217db2fe71738c7102a675f4c187e62c7af018b8
Longhao.Chen <Longhao.Chen@outlook.com>
2021-06-04 00:37:52 +0800

    feat:修改密码产生的算法，此算法与之前的不兼容

--- src/Password.js
+++ src/Password.js
@@ -2,21 +2,18 @@
 const crypto = require('crypto');
 
 function sha512(input) {
-	return crypto.createHash('sha512').update(input).digest('hex');
+	return crypto.createHash('sha512').update(input).digest('base64');
 }
 
 function Password(input) {
-	var pass = sha512(input);
+	var pass = input;
 	//增加暴力破解的计算量，总共计算50次sha512
-	for (var i = 1; i < 50; ++i) {
-		pass = sha512(pass);
+	for (var i = 0; i < 50; ++i) {
+		pass = sha512(input + pass + input);
 	}
 
-	//转成Base64和加入特殊字符，防止某些要求高的地方认为密码强度不够
-	var output = Buffer.from(pass, 'utf-8');
-	output = output.toString('base64');
-	output = output.slice(0, 4) + '-' + output.slice(4, 8) + '-' + output.slice(8, 12);
-	return output;
+	pass = pass.slice(0, 4) + ':' + pass.slice(4, 8) + '$' + pass.slice(8, 12);
+	return pass;
 }
 
 export default Password;
