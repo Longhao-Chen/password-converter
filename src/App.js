a09cdf870b511394992b351e4765211f2217ea1b
Longhao.Chen <Longhao.Chen@outlook.com>
2021-06-05 21:10:05 +0800

    优化界面，添加密码复制

--- src/App.js
+++ src/App.js
@@ -1,31 +1,48 @@
 import './App.css';
 import React from 'react';
+import { CopyToClipboard } from 'react-copy-to-clipboard';
+import { Button } from 'react-bootstrap';
 import Password from './Password';
 
 class HelloMessage extends React.Component {
 	constructor(props) {
 		super(props);
-		this.state = { value: '' };
+		this.state = {
+			value: ''
+		};
 		this.handleChange = this.handleChange.bind(this);
 		this.cleanPassword = this.cleanPassword.bind(this);
 	}
 
 	handleChange(event) {
-		this.setState({ value: event.target.value });
+		this.setState({
+			password: Password(event.target.value),
+			value: event.target.value,
+			copied: false
+		});
 	}
 
 	cleanPassword() {
-		this.setState({ value: '' });
+		this.setState({
+			value: '',
+			password: '',
+			copied: false
+		});
 	}
+
 	render() {
-		var value = this.state.value;
 		return <div>
 			<p>在此输入您的密码：</p>
-			<input autofocus="autofocus" type="password" value={value} onChange={this.handleChange} />
-			<button onClick={this.cleanPassword}>清除密码</button>
+			<input autoFocus="autofocus" type="password" value={this.state.value} onChange={this.handleChange} />
+			<Button variant="danger" onClick={this.cleanPassword}>清除密码</Button>
 			<br />
 			<p>转换后的密码：</p>
-			<input readonly="readonly" type="text" value={Password(value)}  />
+			<input readOnly="readonly" type="text" value={this.state.password} />
+
+			<CopyToClipboard text={this.state.password}
+				onCopy={() => this.setState({ copied: true })}>
+				<Button>{this.state.copied ? "已复制" : "复制密码"}</Button>
+			</CopyToClipboard>
 		</div>;
 	}
 }
@@ -34,6 +51,7 @@ function App() {
 	return (
 		<div className="App">
 			<h1>密码转换器</h1>
+			<hr />
 			<HelloMessage />
 		</div>
 	);
