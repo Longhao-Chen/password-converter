1123e40a4167e2964accdbaeb9f127bc3703f1f9
Longhao.Chen <Longhao.Chen@outlook.com>
2021-06-07 22:07:40 +0800

    fix:拼写错误

--- src/Footer.js
+++ src/Footer.js
@@ -2,17 +2,17 @@ import React from 'react';
 
 function Footer() {
 	return (
-		<dev class="center">
+		<div className="center">
 			<hr />
-			<footer class="footer navbar-fixed-bottom ">
-				<div class="container">
+			<footer className="footer navbar-fixed-bottom ">
+				<div className="container">
 					项目主页及说明文档：
 					<a href="https://github.com/Longhao-Chen/password-converter/">
 						https://github.com/Longhao-Chen/password-converter/
 					</a>
 				</div>
 			</footer>
-		</dev>
+		</div>
 	);
 }
 
