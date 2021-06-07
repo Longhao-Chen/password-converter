6055adc5939061dd19369704d2c3aebacf6ba296
Longhao.Chen <Longhao.Chen@outlook.com>
2021-06-08 00:06:46 +0800

    改进用户界面

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import React from 'react';
 import { CopyToClipboard } from 'react-copy-to-clipboard';
-import { Button } from 'react-bootstrap';
+import { Button, InputGroup } from 'react-bootstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faEye, faEyeSlash } from '@fortawesome/free-solid-svg-icons'
 import Password from './Password';
@@ -35,26 +35,44 @@ class Main extends React.Component {
 	}
 
 	render() {
-		return <div>
-			<p>在此输入您的密码：</p>
-			<input autoFocus="autofocus" type={this.state.showInput?"text":"password"} value={this.state.value} onChange={this.handleChange} ref={this.inputRef} />
-			<FontAwesomeIcon onClick={
-				() => this.setState({ showInput: true ^ this.state.showInput })
-			} icon={this.state.showInput?faEyeSlash:faEye} />
+		return <div className="container col-11 col-md-6 col-lg-5 col-xl-4">
+			<InputGroup>
+				<input placeholder="在此输入您的密码"
+					autoFocus="autofocus"
+					className="form-control"
+					type={this.state.showInput ? "text" : "password"}
+					value={this.state.value}
+					onChange={this.handleChange}
+					ref={this.inputRef} />
+				<span className="input-group-text">
+					<FontAwesomeIcon onClick={
+						() => {
+							this.setState({ showInput: true ^ this.state.showInput });
+							this.inputRef.current.focus();
+						}
+					} icon={this.state.showInput ? faEyeSlash : faEye} />
+				</span>
+				<Button variant="danger" onClick={this.cleanPassword}>清除</Button>
+			</InputGroup>
+			<hr />
 
-			<Button variant="danger" onClick={this.cleanPassword}>清除</Button>
-			<br />
-
-			<p>转换后的密码：</p>
-			<input readOnly="readonly" type={this.state.showOutput?"text":"password"} value={this.state.password} />
-			<FontAwesomeIcon onClick={
-				() => this.setState({ showOutput: true ^ this.state.showOutput })
-			} icon={this.state.showOutput?faEyeSlash:faEye} />
-
-			<CopyToClipboard text={this.state.password}
-				onCopy={() => this.setState({ copied: true })}>
-				<Button>{this.state.copied ? "已复制" : "复制"}</Button>
-			</CopyToClipboard>
+			<h6>转换后的密码为：</h6>
+			<InputGroup>
+				<input placeholder="转换后的密码"
+					readOnly="readonly"
+					className="form-control"
+					type={this.state.showOutput ? "text" : "password"}
+					value={this.state.password} />
+				<span className="input-group-text">
+					<FontAwesomeIcon onClick={
+						() => this.setState({ showOutput: true ^ this.state.showOutput })
+					} icon={this.state.showOutput ? faEyeSlash : faEye} />
+				</span>
+				<CopyToClipboard text={this.state.password}
+					onCopy={() => this.setState({ copied: true })}>
+					<Button>{this.state.copied ? "已复制" : "复制"}</Button>
+				</CopyToClipboard>
+			</InputGroup>
 		</div>;
 	}
 }
